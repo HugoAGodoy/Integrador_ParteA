b3c9fa47de8d38f2bb8e3bbdaa7c47f147d9c239
Hugo Godoy <hugoalbertogodoy@gmail.com>
2025-01-31 22:10:10 -0300

    optimizo, importo foto para pruebas, modifico productos.js

--- src/db/productos.js
+++ src/db/productos.js
@@ -2,35 +2,35 @@ const productos = [
     { 
       id: 1, 
       nombre: "Iphone 11 Pro Max", 
-      foto: "imgs/apple-iphone-11-pro-max.jpg", 
+      foto: "guitars/eddie-van-halen-frankenstrat-electric-guitar.webp", 
       descripcion: "Iphone 11 super pro max", 
       precio: 222.20
     },
     { 
       id: 2, 
       nombre: "IPhone 12", 
-      foto: "imgs/apple-iphone-12.jpg", 
+      foto: "guitars/eddie-van-halen-frankenstrat-electric-guitar.webp",
       descripcion: "Iphone 12, flojito", 
       precio: 333.30
     },
     { 
       id: 3, 
       nombre: "Camara Canon", 
-      foto: "imgs/camara-canon.jpg", 
+      foto:"guitars/eddie-van-halen-frankenstrat-electric-guitar.webp",
       descripcion: "Camara ultra 4k", 
       precio: 444.40
     },
     { 
       id: 4, 
       nombre: "Drone DJI", 
-      foto: "imgs/drone-dji.jpg", 
+      foto: "guitars/eddie-van-halen-frankenstrat-electric-guitar.webp",
       descripcion: "Alto drone", 
       precio: 555.50
     },
     { 
       id: 5, 
       nombre: "Auriculares Sony", 
-      foto: "imgs/auriculares-sony.jpg", 
+      foto: "guitars/eddie-van-halen-frankenstrat-electric-guitar.webp",
       descripcion: "Cancelación activa!", 
       precio: 666.60
     }
